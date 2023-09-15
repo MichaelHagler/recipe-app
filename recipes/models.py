a902a906b7b54ebe2b0f28345f323953a831cb5c
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2023-09-15 15:40:41 -0700

    updating recipe model and urls

--- recipes/models.py
+++ recipes/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
 class Recipe(models.Model):
   name = models.CharField(max_length=120)
+  description = models.TextField(default='If only words were as good as the meal')
   cooking_time = models.FloatField(help_text='in minutes')
   ingredients = models.CharField(max_length=350)
+  pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
 
   def get_absolute_url(self):
     return reverse('recipes:detail', kwargs={'pk': self.pk})
