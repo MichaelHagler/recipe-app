b8f7504e41120f63b177f4273ea550709b3e712a
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2023-09-18 07:10:45 -0700

    fixed spelling on import for Mixins

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView 
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from .models import Recipe 
 
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
   model = Recipe 
   template_name = 'recipes/main.html'
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
   model = Recipe 
   template_name = 'recipes/detail.html'
 
