7d0f64f7df7d55d5296f6efa5c75bdbded6759f3
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2023-09-22 12:19:47 -0700

    no more secret key

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -19,11 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gi#6+b8)&ps(mns8jbhj2wlq$i**lg_v@u^430d-@u6cyty5z1'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
