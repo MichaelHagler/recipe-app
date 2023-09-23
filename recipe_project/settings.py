64e574622d3b308ea2b83e2c46f7137a0c95040a
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2023-09-23 11:57:38 -0700

    Revert "update ALLOWED_HOSTS with site and development server URL"
    This reverts commit 7d5d246d66e7eb5799e368cc8a326395d0540854.

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://salty-tundra-88185-60c59e9f0328.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
