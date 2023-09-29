70636b0e8a2d86f451e4cf0bb2485e4c7930eb79
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2023-09-28 18:03:13 -0700

    allowed hosts

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['salty-tundra-88185-60c59e9f0328.herokuapp.com']
 
 
 # Application definition
